0e3d468037a5ee32f9ae1005e1b45fe8a092cfb0
Matthias Lee <matthias.a.lee@gmail.com>
2012-09-25 02:53:19 -0400

    started implementation of parsin snapshots

--- nmonParser.py
+++ nmonParser.py
@@ -15,16 +15,17 @@ class nmonParser:
 	def parseSysInfo(self):
 		for l in self.rawdata:
 			if "AAA," in l:
+				self.dataPtr+=1
 				# TODO: Strip row heading
 				self.sysInfo.append(l.strip())
-				self.dataPtr+=1
 			else:
 				#if we have finished reading AAA break out 
 				break
+			
 	def parseCols(self):
-		
 		for l in self.rawdata[self.dataPtr:]:
 			if "BBBP," in l:
+				#if we have finished reading headers break
 				break
 			else:
 				self.dataPtr+=1
@@ -33,12 +34,32 @@ class nmonParser:
 				for b in bits[1:]:
 					tmp[b]=[]
 				self.outData[bits[0]]=tmp
-
+				
+	def parseBBBP(self):
+		for l in self.rawdata[self.dataPtr:]:
+			if "BBBP," in l:
+				self.dataPtr+=1
+				self.sysInfo.append(l.strip())
+			else:
+				#if we have finished reading BBBP break out
+				break
+			
+	def parseSnapshots(self):
+		for l in self.rawdata[self.dataPtr:]:
+			if "ZZZZ," in l:
+				#extract time
+				print l
+			else:
+				# split into bits and append
+				bits = l.strip.split(',')
+			
 	def parse(self):
 		# TODO: check fname
 		f = open(self.fname,"r")
 		self.rawdata = f.readlines()
 		self.parseSysInfo()
 		self.parseCols()
+		self.parseBBBP()
+		self.parseSnapshots()
 		print self.outData
 
